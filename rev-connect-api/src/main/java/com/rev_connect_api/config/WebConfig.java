39f80b25c9c955bc2a0b5b7515a8ab18d4824bf4
fifunmitimothy1 <iwasanmio0517@students.bowiestate.edu>
2024-08-16 11:30:45 -0400

    #3-added dependencies to the pom.xml file and created a WebConfig file

---
+++ rev-connect-api/src/main/java/com/rev_connect_api/config/WebConfig.java
@@ -0,0 +1,24 @@
+package com.rev_connect_api.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class WebConfig {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/api/**")
+                        .allowedOrigins("http://localhost:8080")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+
+
+}
